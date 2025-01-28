20aa775889b5b1b0178dc180bc78b6da0c02a37c
Eli Fouts <eligfouts@gmail.com>
2025-01-28 15:25:51 -0500

    Did I do It?

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/EfoutsCode/',
+    plugins: [react()],
+    base: '/EfoutsCode/',
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 })
+
+//import { defineConfig } from 'vite'
+//import react from '@vitejs/plugin-react'
+//
+//// https://vite.dev/config/
+//export default defineConfig({
+//  plugins: [react()],
+//  base: '/EfoutsCode/',
+//})
