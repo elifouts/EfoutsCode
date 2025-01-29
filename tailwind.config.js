4d5789042b7a22aac5091cc5e441bfbb367c33ee
Eli Fouts <eligfouts@gmail.com>
2025-01-28 21:03:55 -0500

    added wave. Settup for tailwind.

--- tailwind.config.js
+++ tailwind.config.js
@@ -50,6 +50,10 @@ module.exports = {
   				'4': 'hsl(var(--chart-4))',
   				'5': 'hsl(var(--chart-5))'
   			}
+  		},
+  		backgroundImage: {
+  			'grid-pattern': ',
+  			'grid-pattern-light': '
   		}
   	}
   },
